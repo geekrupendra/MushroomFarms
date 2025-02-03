edf58b247e850a152037fd844303a0564e3b2d91
Rupendra Sahu <159014325+geekrupendra@users.noreply.github.com>
2025-02-03 22:43:47 +0530

    Add files via upload

---
+++ vedika_farms/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vedika_farms project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vedika_farms.settings')
+
+application = get_wsgi_application()
